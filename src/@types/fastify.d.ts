a78c5f8376002e04c402d2193b7b11d9d574d3e5
gabrielmatsan <gabriel22070059@aluno.cesupa.br>
2024-09-12 23:41:00 -0300

    update to check cookies session_id

---
+++ src/@types/fastify.d.ts
@@ -0,0 +1,21 @@
+import 'fastify'
+
+declare module 'fastify' {
+  export interface FastifyRequest {
+    user?: {
+      id: string
+
+      name: string
+
+      email: string
+
+      session_id: string
+
+      created_at: string
+
+      updated_at: string
+
+      password: string
+    }
+  }
+}
